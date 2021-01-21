147fa766e90373a2674490baf37bc6cd5d1108e9
Sahil <56181733+skassanjee@users.noreply.github.com>
2021-01-20 22:35:28 -0700

    updated controllers

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require ('express')
 
 const app = express()
 
+const authRoutes = require('./routes/authroutes')
+
 app.use(express.static('public'))
+app.use(express.json())
 
 app.set('view engine', 'ejs');
 
 
-
+app.use(authRoutes)
 
 app.get('/', (req, res) => res.render('home'));
 
